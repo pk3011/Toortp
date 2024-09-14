369fd3be4c48430b63823cddc1bdb9ee98227764
LankeshRavan66 <159647350+LankeshRavan66@users.noreply.github.com>
2024-09-14 13:45:07 +0530

    Add files via upload

--- api/index.php
+++ api/index.php
@@ -1,63 +1,11 @@
 <?php
 error_reporting(0);
 ini_set('display_errors', 0);
-function fetchData(string $url): ?string {
-    return ($data = @file_get_contents($url)) !== false ? trim($data) : null;
-}
-function fetchMPDManifest(string $url): ?string {
-    $curl = curl_init($url);
-    curl_setopt_array($curl, [
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_HTTPHEADER => [
-            'X-Forwarded-For: 59.178.72.184'
-        ],
-    ]);
-    $content = curl_exec($curl);
-    curl_close($curl);
-    return $content !== false ? $content : null;
-}
-function extractPsshFromManifest(string $content, string $baseUrl): ?array {
-    if (($xml = @simplexml_load_string($content)) === false) return null;
-    foreach ($xml->Period->AdaptationSet as $set) {
-        if ((string)$set['contentType'] === 'audio') {
-            foreach ($set->Representation as $rep) {
-                $template = $rep->SegmentTemplate ?? null;
-                if ($template) {
-                    $media = str_replace(['$RepresentationID$', '$Number$'], [(string)$rep['id'], (int)($template['startNumber'] ?? 0) + (int)($template->SegmentTimeline->S['r'] ?? 0)], $template['media']);
-                    $url = "$baseUrl/dash/$media";
-                    $context = stream_context_create([
-                        'http' => ['method' => 'GET', 'header' => 'X-Forwarded-For: 59.178.72.184'],
-                    ]);
-                    if (($content = @file_get_contents($url, false, $context)) !== false) {
-                        $hex = bin2hex($content);
-                        $marker = "000000387073736800000000edef8ba979d64acea3c827dcd51d21ed000000";
-                        if (($pos = strpos($hex, $marker)) !== false && ($end = strpos($hex, "0000", $pos + strlen($marker))) !== false) {
-                            $psshHex = substr($hex, $pos, $end - $pos - 12);
-                            $psshHex = str_replace("000000387073736800000000edef8ba979d64acea3c827dcd51d21ed00000018", "000000327073736800000000edef8ba979d64acea3c827dcd51d21ed00000012", $psshHex);
-                            $kidHex = substr($psshHex, 68, 32);
-                            return [
-                                'pssh' => base64_encode(hex2bin($psshHex)),
-                                'kid' => substr($kidHex, 0, 8) . "-" . substr($kidHex, 8, 4) . "-" . substr($kidHex, 12, 4) . "-" . substr($kidHex, 16, 4) . "-" . substr($kidHex, 20)
-                            ];
-                        }
-                    }
-                }
-            }
-        }
-    }
-    return null;
-}
-function getChannelInfo(string $id): array {
-    $json = @file_get_contents('https://raw.githubusercontent.com/ttoor5/tataplay_urls/main/origin.json');
-    $channels = $json !== false ? json_decode($json, true) : null;
-    if ($channels === null) {
-        exit;
-    }
-    foreach ($channels as $channel) {
-        if ($channel['id'] == $id) return $channel;
-    }
-    exit;
-}
+$userAgent = $_SERVER['HTTP_USER_AGENT'];
+$beginTimestamp = isset($_GET['utc']) ? intval($_GET['utc']) : null;
+$endTimestamp = isset($_GET['lutc']) ? intval($_GET['lutc']) : null;
+$begin = $beginTimestamp ? date('Ymd\THis', $beginTimestamp) : 'unknown';
+$end = $endTimestamp ? date('Ymd\THis', $endTimestamp) : 'unknown';
 $id = $_GET['id'] ?? exit;
 $channelInfo = getChannelInfo($id);
 $dashUrl = $channelInfo['streamData']['MPD='] ?? exit;
@@ -65,11 +13,15 @@ if (strpos($dashUrl, 'https://bpprod') !== 0) {
     header("Location: $dashUrl");
     exit;
 }
-$manifestContent = fetchMPDManifest($dashUrl) ?? exit;
+if ($beginTimestamp) {
+    $dashUrl = str_replace('master', 'manifest', $dashUrl);
+    $dashUrl .= "?begin=$begin&end=$end";
+}
+$manifestContent = fetchMPDManifest($dashUrl, $userAgent) ?? exit;
 $baseUrl = dirname($dashUrl);
-$widevinePssh = extractPsshFromManifest($manifestContent, $baseUrl);
-$processedManifest = str_replace('dash/', "$baseUrl/dash/", $manifestContent);
-if ($widevinePssh) {
+$widevinePssh = extractPsshFromManifest($manifestContent, $baseUrl, $userAgent, $beginTimestamp);
+    $processedManifest = str_replace('dash/', "$baseUrl/dash/", $manifestContent);
+    if ($widevinePssh) {
     $processedManifest = str_replace(
       '<ContentProtection value="cenc" schemeIdUri="urn:mpeg:dash:mp4protection:2011"/>',
       '<!-- Common Encryption -->
@@ -95,10 +47,78 @@ if (in_array($id, ['244', '599'])) {
     );
     $processedManifest = preg_replace('/<Representation id="video=3187600" bandwidth="3187600".*?<\/Representation>/s', '', $processedManifest);
 }
+
+header('Content-Security-Policy: default-src \'self\';');
+header('X-Content-Type-Options: nosniff');
+header('X-Frame-Options: DENY');
+header('X-XSS-Protection: 1; mode=block');
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header('Content-Type: application/dash+xml');
+header("Cache-Control: max-age=20, public");
 header('Content-Disposition: attachment; filename="script_by_drmlive' . urlencode($id) . '.mpd"');
 echo $processedManifest;
+function fetchMPDManifest(string $url, string $userAgent): ?string {
+    $context = stream_context_create([
+        'http' => [
+            'method' => 'GET',
+            'header' => 'User-Agent: ' . $userAgent,
+        ]
+    ]);
+    $content = @file_get_contents($url, false, $context);
+    return $content !== false ? $content : null;
+}
+function extractKid($hexContent) {
+    $psshMarker = "70737368";
+    $psshOffset = strpos($hexContent, $psshMarker);
+    
+    if ($psshOffset !== false) {
+        $headerSizeHex = substr($hexContent, $psshOffset - 8, 8);
+        $headerSize = hexdec($headerSizeHex);
+        $psshHex = substr($hexContent, $psshOffset - 8, $headerSize * 2);
+        $kidHex = substr($psshHex, 68, 32);
+        $newPsshHex = "000000327073736800000000edef8ba979d64acea3c827dcd51d21ed000000121210" . $kidHex;
+        $pssh = base64_encode(hex2bin($newPsshHex));
+        $kid = substr($kidHex, 0, 8) . "-" . substr($kidHex, 8, 4) . "-" . substr($kidHex, 12, 4) . "-" . substr($kidHex, 16, 4) . "-" . substr($kidHex, 20);
+        
+        return ['pssh' => $pssh, 'kid' => $kid];
+    }
+    
+    return null;
+}
+function extractPsshFromManifest(string $content, string $baseUrl, string $userAgent, ?int $beginTimestamp): ?array {
+    if (($xml = @simplexml_load_string($content)) === false) return null;
+    foreach ($xml->Period->AdaptationSet as $set) {
+        if ((string)$set['contentType'] === 'audio') {
+            foreach ($set->Representation as $rep) {
+                $template = $rep->SegmentTemplate ?? null;
+                if ($template) {
+                    $startNumber = $beginTimestamp ? (int)($template['startNumber'] ?? 0) : (int)($template['startNumber'] ?? 0) + (int)($template->SegmentTimeline->S['r'] ?? 0);
+                    $media = str_replace(['$RepresentationID$', '$Number$'], [(string)$rep['id'], $startNumber], $template['media']);
+                    $url = "$baseUrl/dash/$media";
+                    $context = stream_context_create([
+                        'http' => ['method' => 'GET', 'header' => 'User-Agent: ' . $userAgent],
+                    ]);
+                    if (($content = @file_get_contents($url, false, $context)) !== false) {
+                        $hexContent = bin2hex($content);
+                        return extractKid($hexContent);
+                    }
+                }
+            }
+        }
+    }
+    return null;
+}
+function getChannelInfo(string $id): array {
+    $json = @file_get_contents('https://raw.githubusercontent.com/ttoor5/tataplay_urls/main/origin.json');
+    $channels = $json !== false ? json_decode($json, true) : null;
+    if ($channels === null) {
+        exit;
+    }
+    foreach ($channels as $channel) {
+        if ($channel['id'] == $id) return $channel;
+    }
+    exit;
+}
 ?>
